909764fb7452dc8e7236b18a24170f51ab0d1a15
Axcel Lucas de Mello <88753512+axcelmello@users.noreply.github.com>
2022-04-06 15:05:14 -0300

    Correção de Classes Banco - Fields e Opções

--- Qualifier/models.py
+++ Qualifier/models.py
@@ -2,29 +2,44 @@ from django.db import models
 
 # Create your models here.
 
-
 class Cadastro_empresa(models.Model):
-    nome_fantasia     = models.CharField(max_length=120)
-    razao_social      = models.CharField(max_length=120)
-    cnpj              = models.CharField(max_length=120)
-    telefone          = models.CharField(max_length=120)
-    email             = models.CharField(max_length=120)
-    site              = models.CharField(max_length=120)
-    endereco          = models.CharField(max_length=120)
-    pais_sede         = models.CharField(max_length=120)
+    nome_fantasia     = models.CharField(max_length=50)
+    razao_social      = models.CharField(max_length=50)
+    cnpj              = models.IntegerField()
+    telefone          = models.IntegerField()
+    email             = models.EmailField(max_length=25)
+    site              = models.CharField(max_length=25)
+    endereco          = models.CharField(max_length=30)
+    pais_sede         = models.CharField(max_length=25)
 
 class Cadastro_contato(models.Model):
-    nome              = models.CharField(max_length=120)
-    nomeempresa       = models.CharField(max_length=120)
-    cargo             = models.CharField(max_length=120)
-    telefone          = models.CharField(max_length=120)
-    email             = models.CharField(max_length=120)
+    nome              = models.CharField(max_length=50)
+    nomeempresa       = models.ForeignKey(Cadastro_empresa, on_delete=models.CASCADE)
+    cargo             = models.CharField(max_length=25)
+    telefone          = models.IntegerField()
+    email             = models.EmailField(max_length=25)
 
 class Dados_empresa(models.Model):
-    cnpj              = models.CharField(max_length=120)    #info de vinculação com classe Cadastro_empresa
-    n_funcionarios    = models.CharField(max_length=120)    #definir tamanho da empresa?
-    setor             = models.CharField(max_length=120)    #primário, secundário, terciário
+    SETOR = (
+        ('Primário', 'Primário'),
+        ('Secundário', 'Secundário'),
+        ('Terciário', 'Terciário'),
+    )
+    CAPITAL = (
+        ('Privado', 'Privado'),
+        ('Público', 'Público'),
+        ('Misto', 'Misto'),
+    )
+    ABRANGENCIA = (
+        ('Municipal', 'Municipal'),
+        ('Regional', 'Regional'),
+        ('Estadual', 'Estadual'),
+        ('Nacional', 'Nacional'),
+        ('Multinacional', 'Multinacional'),
+    )
+    cnpj              = models.ForeignKey(Cadastro_empresa, on_delete=models.CASCADE)
+    n_funcionarios    = models.IntegerField()
+    setor             = models.CharField(max_length=15, choices=SETOR)
     categoria         = models.CharField(max_length=120)    #tecnologia, saúde, produtos, industria
-    capital           = models.CharField(max_length=120)    #privado, público, cooperativa, associação, misto etc.
-    abrangencia       = models.CharField(max_length=120)    #Municipal, Regional, Estadual, Nacional, Multinacional
-
+    capital           = models.CharField(max_length=15, choices=CAPITAL)
+    abrangencia       = models.CharField(max_length=25, choices=ABRANGENCIA)
