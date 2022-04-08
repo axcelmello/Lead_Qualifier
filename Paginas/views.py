166311b75fb088aa4c2c5942de1db57f22f70a04
Axcel Lucas de Mello <88753512+axcelmello@users.noreply.github.com>
2022-04-08 20:16:39 -0300

    Padrão de Formulário Django

--- Paginas/views.py
+++ Paginas/views.py
@@ -4,7 +4,7 @@ from Qualifier.models import Cadastro_empresa
 from Qualifier.models import Cadastro_contato
 from Qualifier.models import Dados_empresa
 
-from Qualifier.forms import Contato_form
+from Qualifier.forms import Contato_form, Empresa_form
 # Create your views here.
 
 # print(request.POST)
@@ -17,10 +17,15 @@ def painel(request, *args, **kwargs):
     return render(request, "Painel.html", {})
 
 def cadastro_empresa(request, *args, **kwargs):
+    form = Empresa_form
+    context = {
+        "form": form,
+    }
+
     if request.method == 'POST':
         print(request.POST.get)
 
-    return render(request, "Cadastro_empresa.html", {})
+    return render(request, "Cadastro_empresa.html", context)
 
 def cadastro_contato(request, *args, **kwargs):
     form = Contato_form
--- Qualifier/forms.py
+++ Qualifier/forms.py
@@ -6,3 +6,13 @@ class Contato_form(forms.Form):
     cargo         = forms.CharField()
     telefone      = forms.IntegerField()
     email         = forms.EmailField()
+
+class Empresa_form(forms.Form):
+    nome_fantasia = forms.CharField()
+    razao_social = forms.CharField()
+    cnpj = forms.IntegerField()
+    telefone = forms.IntegerField()
+    email = forms.EmailField()
+    site = forms.CharField()
+    endereco = forms.CharField()
+    pais_sede = forms.CharField()
