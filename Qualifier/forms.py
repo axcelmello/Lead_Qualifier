e11f4882dde8853b11e74a153713a157d827482a
Axcel Lucas de Mello <88753512+axcelmello@users.noreply.github.com>
2022-05-15 17:22:02 -0300

    .

--- Qualifier/forms.py
+++ Qualifier/forms.py
@@ -11,15 +11,33 @@ class Empresa_form(forms.Form):
     nome_fantasia = forms.CharField()
     razao_social = forms.CharField()
     cnpj = forms.CharField()
-    telefone = forms.IntegerField()
+    telefone = forms.CharField()
     email = forms.EmailField()
     site = forms.CharField()
     endereco = forms.CharField()
     pais_sede = forms.CharField()
 
+    SETOR = (
+        ('Primário', 'Primário'),
+        ('Secundário', 'Secundário'),
+        ('Terciário', 'Terciário'),
+    )
+    CAPITAL = (
+        ('Privado', 'Privado'),
+        ('Público', 'Público'),
+        ('Misto', 'Misto'),
+    )
+    ABRANGENCIA = (
+        ('Municipal', 'Municipal'),
+        ('Regional', 'Regional'),
+        ('Estadual', 'Estadual'),
+        ('Nacional', 'Nacional'),
+        ('Multinacional', 'Multinacional'),
+    )
+
     #Dados empresa
     n_funcionarios = forms.IntegerField()
-    setor = forms.CharField()
+    setor = forms.ChoiceField(choices=SETOR)
     categoria = forms.CharField()
-    capital = forms.CharField()
-    abrangencia = forms.CharField()
+    capital = forms.ChoiceField(choices=CAPITAL)
+    abrangencia = forms.ChoiceField(choices=ABRANGENCIA)
