f4d17ec015cf1a43db75c819804f544b6dc0ace2
Axcel Lucas de Mello <88753512+axcelmello@users.noreply.github.com>
2022-06-10 21:29:56 -0300

    Alteração Banco de Dados

--- Qualifier/forms.py
+++ Qualifier/forms.py
@@ -1,45 +1,49 @@
+from asyncio.windows_events import NULL
 from django import forms
 from localflavor.br.forms import BRCNPJField, BRCNPJValidator
 
 from Qualifier.models import Cadastro_empresa
 
 class Contato_form(forms.Form):
-    nome_contato          = forms.CharField(label="Nome do Contato")
-    nome_empresa_contato  = forms.ModelChoiceField(label="Empresa do Contato",queryset=Cadastro_empresa.objects.values_list('nome_fantasia',flat=True).distinct())
-    cargo_contato         = forms.CharField(label="Cargo do Contato")
-    telefone_contato      = forms.CharField(label="Telefone do Contato")
-    email_contato         = forms.EmailField(label="E-mail do Contato")
+    nome_contato          = forms.CharField()
+    nome_empresa_contato  = forms.ModelChoiceField(queryset=Cadastro_empresa.objects.values_list('nome_fantasia',flat=True).distinct())
+    cargo_contato         = forms.CharField(required=False)
+    telefone_contato      = forms.CharField(required=False)
+    email_contato         = forms.EmailField(required=False)
 
 class Empresa_form(forms.Form):
-    nome_fantasia = forms.CharField(label="Nome Fantasia")
-    razao_social = forms.CharField(label="Razão Social")
-    cnpj = BRCNPJField(label="CNPJ", max_length=14,validators=[BRCNPJValidator])
-    telefone_empresa = forms.CharField(label="Telefone da Empresa")
-    email_empresa = forms.EmailField(label="E-mail Empresa")
-    site = forms.CharField(label="Site")
-    endereco = forms.CharField(label="Endereço")
-    pais_sede = forms.CharField(label="Pais Sede")
+    nome_fantasia = forms.CharField()
+    razao_social = forms.CharField(required=False)
+    cnpj = BRCNPJField(max_length=14,validators=[BRCNPJValidator])
+    telefone_empresa = forms.CharField()
+    email_empresa = forms.EmailField(required=False)
+    site = forms.CharField(required=False)
+    endereco = forms.CharField(required=False)
+    pais_sede = forms.CharField(required=False)
 
     SETOR = (
-        ('Primário', 'Primário'),
-        ('Secundário', 'Secundário'),
-        ('Terciário', 'Terciário'),
+        ('', 'Selecione...'),
+        ('1', 'Primário'),
+        ('2', 'Secundário'),
+        ('3', 'Terciário'),
     )
     CAPITAL = (
-        ('Privado', 'Privado'),
-        ('Público', 'Público'),
-        ('Misto', 'Misto'),
+        ('', 'Selecione...'),
+        ('1', 'Privado'),
+        ('2', 'Público'),
+        ('3', 'Misto'),
     )
     ABRANGENCIA = (
-        ('Municipal', 'Municipal'),
-        ('Regional', 'Regional'),
-        ('Estadual', 'Estadual'),
-        ('Nacional', 'Nacional'),
-        ('Multinacional', 'Multinacional'),
+        ('', 'Selecione...'),
+        ('1', 'Municipal'),
+        ('2', 'Regional'),
+        ('3', 'Estadual'),
+        ('4', 'Nacional'),
+        ('5', 'Multinacional'),
     )
     
-    n_funcionarios = forms.IntegerField()
-    setor = forms.ChoiceField(choices=SETOR)
-    categoria = forms.CharField()
-    capital = forms.ChoiceField(choices=CAPITAL)
-    abrangencia = forms.ChoiceField(choices=ABRANGENCIA)
+    n_funcionarios = forms.IntegerField(required=False,initial=0)
+    setor = forms.ChoiceField(choices=SETOR,required=False)
+    categoria = forms.CharField(required=False)
+    capital = forms.ChoiceField(required=False,choices=CAPITAL)
+    abrangencia = forms.ChoiceField(required=False,choices=ABRANGENCIA)
--- Qualifier/models.py
+++ Qualifier/models.py
@@ -5,33 +5,36 @@ from localflavor.br.models import BRCNPJField
 
 class Cadastro_empresa(models.Model):
     nome_fantasia             = models.CharField(max_length=65)
-    razao_social              = models.CharField(blank=True,max_length=65)
-    cnpj                      = BRCNPJField(blank=True,max_length=14,unique=True)
-    telefone_empresa          = models.CharField(blank=True,max_length=14)
-    email_empresa             = models.EmailField(blank=True,max_length=65)
-    site                      = models.CharField(blank=True,max_length=65)
-    endereco                  = models.CharField(blank=True,max_length=65)
-    pais_sede                 = models.CharField(blank=True,max_length=65)
+    razao_social              = models.CharField(max_length=65)
+    cnpj                      = BRCNPJField(max_length=14,unique=True)
+    telefone_empresa          = models.CharField(max_length=14)
+    email_empresa             = models.EmailField(max_length=65)
+    site                      = models.CharField(max_length=65)
+    endereco                  = models.CharField(max_length=65)
+    pais_sede                 = models.CharField(max_length=65)
     SETOR = (
-        ('Primário', 'Primário'),
-        ('Secundário', 'Secundário'),
-        ('Terciário', 'Terciário'),
+        ('', 'Selecione...'),
+        ('1', 'Primário'),
+        ('2', 'Secundário'),
+        ('3', 'Terciário'),
     )
     CAPITAL = (
-        ('Privado', 'Privado'),
-        ('Público', 'Público'),
-        ('Misto', 'Misto'),
+        ('', 'Selecione...'),
+        ('1', 'Privado'),
+        ('2', 'Público'),
+        ('3', 'Misto'),
     )
     ABRANGENCIA = (
-        ('Municipal', 'Municipal'),
-        ('Regional', 'Regional'),
-        ('Estadual', 'Estadual'),
-        ('Nacional', 'Nacional'),
-        ('Multinacional', 'Multinacional'),
+        ('', 'Selecione...'),
+        ('1', 'Municipal'),
+        ('2', 'Regional'),
+        ('3', 'Estadual'),
+        ('4', 'Nacional'),
+        ('5', 'Multinacional'),
     )
-    n_funcionarios    = models.IntegerField(blank=True,)
+    n_funcionarios    = models.IntegerField(null=True)
     setor             = models.CharField(max_length=65, choices=SETOR)
-    categoria         = models.CharField(blank=True,max_length=120)    #tecnologia, saúde, produtos, industria
+    categoria         = models.CharField(max_length=120)
     capital           = models.CharField(max_length=65, choices=CAPITAL)
     abrangencia       = models.CharField(max_length=65, choices=ABRANGENCIA)
 
@@ -44,9 +47,9 @@ class Cadastro_contato(models.Model):
     nome_contato              = models.CharField(max_length=65)
     id_empresa_FK             = models.ForeignKey('Cadastro_empresa', on_delete=models.CASCADE, null=True)
     nome_empresa_contato      = models.CharField(max_length=65)
-    cargo_contato             = models.CharField(blank=True,max_length=65)
-    telefone_contato          = models.CharField(blank=True,max_length=14)
-    email_contato             = models.EmailField(blank=True,max_length=65)
+    cargo_contato             = models.CharField(max_length=65)
+    telefone_contato          = models.CharField(max_length=14)
+    email_contato             = models.EmailField(max_length=65)
 
     def __str__(self):
         return self.nome_contato
